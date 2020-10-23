0ba25f526ecc479be9637721b1eb97a797398a0b
ChristianLievrouw <celievrouw1@gmail.com>
2020-10-22 20:26:01 -0500

    Login/Sign up button

--- lib/ui/widgets/textfield_widget.dart
+++ lib/ui/widgets/textfield_widget.dart
@@ -19,17 +19,37 @@ class TextFieldWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      onChanged: onChanged,
+      obscureText: obscureText,
+      style: TextStyle(
+        color: Global.mediumBlue,
+        fontSize: 14.0,
+      ),
+      cursorColor: Global.mediumBlue,
         decoration: InputDecoration(
-      hintText: hintText,
+      labelText: hintText,
       prefixIcon: Icon(
         prefixIconData,
         size: 18,
         color: Global.mediumBlue,
       ),
+      filled: true,
       enabledBorder: UnderlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide.none,
       ),
-    ));
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Global.mediumBlue),
+      ),
+      suffixIcon: Icon(
+        suffexIconData,
+        size: 18,
+        color: Global.mediumBlue,
+      ),
+labelStyle: TextStyle(color: Global.mediumBlue),
+focusColor: Global.mediumBlue,
+    ),
+    );
   }
 }
